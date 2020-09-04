68e2a5f3d37ecab7c9d96b3d1735ebb915e8feac
Larry Zhu <larry.zhu@oracle.com>
2020-09-03 20:21:42 -0700

    working

--- src/plugins/shard_map/public/components/app.tsx
+++ src/plugins/shard_map/public/components/app.tsx
@@ -16,111 +16,259 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-import React, { useState } from 'react';
 import { i18n } from '@kbn/i18n';
-import { FormattedMessage, I18nProvider } from '@kbn/i18n/react';
+import { I18nProvider } from '@kbn/i18n/react';
+import { EuiPage, EuiPageBody, EuiPageContent, EuiPageContentBody } from '@elastic/eui';
+import React, { Fragment, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import {
-  EuiButton,
-  EuiHorizontalRule,
-  EuiPage,
-  EuiPageBody,
-  EuiPageContent,
-  EuiPageContentBody,
-  EuiPageContentHeader,
-  EuiPageHeader,
-  EuiTitle,
-  EuiText,
+  EuiToolTip,
+  EuiDragDropContext,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiDraggable,
+  EuiDroppable,
+  EuiPanel,
+  EuiIconTip,
+  EuiSpacer,
+  EuiFieldText,
+  EuiInMemoryTable,
 } from '@elastic/eui';
-
-import { CoreStart } from '../../../../../src/core/public';
+import { htmlIdGenerator } from '@elastic/eui/lib/services';
+import { ApolloClient, useQuery, useMutation, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { LIST_NODES, LIST_SHARDS, MOVE_SHARD } from './query';
+import { PLUGIN_ID } from '../../common';
 import { NavigationPublicPluginStart } from '../../../../../src/plugins/navigation/public';
-
-import { PLUGIN_ID, PLUGIN_NAME } from '../../common';
-
-interface ShardMapAppDeps {
+import { CoreStart } from '../../../../../src/core/public';
+const makeId = htmlIdGenerator();
+export const loadData = (nodes: any, shards: any) => {
+  const indexMap = new Map();
+  const nodeMap = new Map();
+  for (let i = 0; i < shards.length; i++) {
+    const shard = shards[i];
+    if (!(shard.node in nodeMap)) {
+      nodeMap.set(shard.node, 0);
+    }
+    if (!(shard.index in indexMap)) {
+      indexMap.set(shard.index, 0);
+    }
+    nodeMap.set(shard.node, nodeMap.get(shard.node) + shard.indexingRate);
+    indexMap.set(shard.index, indexMap.get(shard.index) + shard.indexingRate);
+  }
+  const nodesArray: any = [];
+  nodes.forEach((node: any) => {
+    let value = 0;
+    if (nodeMap.has(node.name)) {
+      value = Math.round(nodeMap.get(node.name) / 60) * 1.0;
+    }
+    nodesArray.push({ node: node.name, indexingRate: value > 0 ? value : 0 });
+  });
+  const indicesArray: any = [];
+  indexMap.forEach((value, key, map) =>
+    indicesArray.push({
+      index: key,
+      indexingRate: value > 0 ? Math.round(value / 60) * 1.0 : 0,
+    })
+  );
+  return [nodesArray, indicesArray];
+};
+const Node = ({ node, shards }) => {
+  return (
+    <EuiToolTip position="right" content={node.name}>
+      <EuiFlexItem>
+        <EuiDroppable
+          key={node.name}
+          droppableId={'DROPPABLE_AREA_TYPE_' + node.name}
+          type="TYPE_ONE"
+          spacing="m"
+          withPanel
+        >
+          {shards.map((shard, idx) => (
+            <Shard index={idx} key={makeId()} shard={shard} />
+          ))}
+        </EuiDroppable>
+      </EuiFlexItem>
+    </EuiToolTip>
+  );
+};
+const getColor = (state) => {
+  switch (state) {
+    case 'STARTED':
+      return '#017D73';
+    case 'RELOCATING':
+      return '#F1D86F';
+    case 'UNASSIGNED':
+      return '#E7664C';
+    default:
+      return '#017D73';
+  }
+};
+const Shard = ({ index, shard }) => {
+  return (
+    <EuiDraggable
+      key={shard.id}
+      index={index}
+      draggableId={`${shard.node}/${shard.index}/${shard.shard}`}
+      spacing="m"
+    >
+      {(provided, state) => (
+        <EuiToolTip
+          position="right"
+          content={'name ' + shard.name + ' id ' + shard.id + ' state ' + shard.state}
+        >
+          <EuiPanel key={makeId()} hasShadow={state.isDragging}>
+            <div style={{ color: getColor(shard.state) }}>
+              {shard.index + ':' + shard.shard + ':' + shard.prirep}
+              {state.isDragging && ' ✨'}
+            </div>
+          </EuiPanel>
+        </EuiToolTip>
+      )}
+    </EuiDraggable>
+  );
+};
+const Table2 = ({ data }) => {
+  const columns = [
+    {
+      field: 'node',
+      name: 'Node',
+      sortable: true,
+    },
+    {
+      name: 'Indexing Rate',
+      field: 'indexingRate',
+      sortable: true,
+    },
+  ];
+  const sorting = {
+    sort: {
+      field: 'indexingRate',
+      direction: 'desc',
+    },
+  };
+  return <EuiInMemoryTable items={data} columns={columns} pagination={false} sorting={sorting} />;
+};
+const Table3 = ({ data }) => {
+  const columns = [
+    {
+      field: 'index',
+      name: 'Index',
+      sortable: true,
+    },
+    {
+      field: 'indexingRate',
+      name: 'Indexing Rate',
+      sortable: true,
+    },
+  ];
+  const sorting = {
+    sort: {
+      field: 'indexingRate',
+      direction: 'desc',
+    },
+  };
+  return <EuiInMemoryTable items={data} columns={columns} pagination={false} sorting={sorting} />;
+};
+export const Table = () => {
+  const [indexName, setIndexName] = useState('.monitoring-kibana*');
+  const onChangeIndexName = (e) => {
+    setIndexName(e.target.value);
+  };
+  const onDragEnd = async ({ source, destination, draggableId }) => {
+    const [node, index, shard] = draggableId.split('/');
+    const target = destination.droppableId.substring('DROPPABLE_AREA_TYPE_'.length);
+    // console.log("target", target);
+    if (source && destination && source.droppableId !== destination.droppableId) {
+      // console.log(`moving ${index} ${shard} from ${node} to ${target}`);
+      const res = await moveShard({
+        variables: { shard, index, toNode: target, fromNode: node },
+        update: (cache) => {
+          // console.log('moved');
+        },
+      });
+      // console.log(res.data);
+    }
+  };
+  const { loading: loadingNodes, error: errorNodes, data: dataNodes } = useQuery(LIST_NODES);
+  const { loading: loadingShards, error: errorShards, data: dataShards } = useQuery(LIST_SHARDS, {
+    variables: {
+      filter: indexName,
+    },
+  });
+  const [moveShard] = useMutation(MOVE_SHARD);
+  if (loadingNodes) return <p>Loading nodes ...</p>;
+  if (errorNodes) return <p>`Error in loading nodes! {errorNodes.message}`</p>;
+  if (loadingShards) return <p>Loading shards ...</p>;
+  if (errorShards) return <p>`Error in loading shards! {errorShards.message}`</p>;
+  const shardMap = {};
+  for (let i = 0; i < dataShards.shards.length; i++) {
+    const shard = dataShards.shards[i];
+    if (!(shard.node in shardMap)) {
+      shardMap[shard.node] = [];
+    }
+    shardMap[shard.node].push(shard);
+  }
+  const [nodeData, indexData] = loadData(dataNodes.nodes, dataShards.shards);
+  return (
+    <Fragment>
+      <EuiFieldText
+        key="textfield-0"
+        fullWidth
+        placeholder="cim-*"
+        value={indexName}
+        onChange={(e) => onChangeIndexName(e)}
+        append={[<EuiIconTip key={makeId()} content="index" />]}
+        aria-label="Use aria labels when no actual label is in use"
+      />
+      <EuiSpacer key={makeId()} size="m" />
+      <div>
+        <EuiDragDropContext key={makeId()} onDragEnd={onDragEnd}>
+          <EuiFlexGroup key={makeId()} wrap={true}>
+            {dataNodes.nodes.map((node, idx) => (
+              <Node idx={idx} key={makeId()} node={node} shards={shardMap[node.name] || []} />
+            ))}
+          </EuiFlexGroup>
+        </EuiDragDropContext>
+      </div>
+      <EuiSpacer key={makeId()} size="m" />
+      <Table2 data={nodeData} />
+      <EuiSpacer key={makeId()} size="m" />
+      <Table3 data={indexData} />
+    </Fragment>
+  );
+};
+interface ShardmapAppDeps {
   basename: string;
   notifications: CoreStart['notifications'];
   http: CoreStart['http'];
   navigation: NavigationPublicPluginStart;
 }
-
-export const ShardMapApp = ({ basename, notifications, http, navigation }: ShardMapAppDeps) => {
+export const ShardMapApp = ({ basename, notifications, http, navigation }: ShardmapAppDeps) => {
   // Use React hooks to manage state.
-  const [timestamp, setTimestamp] = useState<string | undefined>();
-
-  const onClickHandler = () => {
-    // Use the core http service to make a response to the server API.
-    http.get('/api/shard_map/example').then((res) => {
-      setTimestamp(res.time);
-      // Use the core notifications service to display a success message.
-      notifications.toasts.addSuccess(
-        i18n.translate('shardMap.dataUpdated', {
-          defaultMessage: 'Data updated',
-        })
-      );
-    });
-  };
-
+  const client = new ApolloClient({
+    uri: 'http://localhost:8080/query',
+    cache: new InMemoryCache(),
+  });
   // Render the application DOM.
   // Note that `navigation.ui.TopNavMenu` is a stateful component exported on the `navigation` plugin's start contract.
   return (
     <Router basename={basename}>
-      <I18nProvider>
-        <>
-          <navigation.ui.TopNavMenu appName={PLUGIN_ID} showSearchBar={true} />
-          <EuiPage restrictWidth="1000px">
-            <EuiPageBody>
-              <EuiPageHeader>
-                <EuiTitle size="l">
-                  <h1>
-                    <FormattedMessage
-                      id="shardMap.helloWorldText"
-                      defaultMessage="{name}"
-                      values={{ name: PLUGIN_NAME }}
-                    />
-                  </h1>
-                </EuiTitle>
-              </EuiPageHeader>
-              <EuiPageContent>
-                <EuiPageContentHeader>
-                  <EuiTitle>
-                    <h2>
-                      <FormattedMessage
-                        id="shardMap.congratulationsTitle"
-                        defaultMessage="Congratulations, you have successfully created a new Kibana Plugin!"
-                      />
-                    </h2>
-                  </EuiTitle>
-                </EuiPageContentHeader>
-                <EuiPageContentBody>
-                  <EuiText>
-                    <p>
-                      <FormattedMessage
-                        id="shardMap.content"
-                        defaultMessage="Look through the generated code and check out the plugin development documentation."
-                      />
-                    </p>
-                    <EuiHorizontalRule />
-                    <p>
-                      <FormattedMessage
-                        id="shardMap.timestampText"
-                        defaultMessage="Last timestamp: {time}"
-                        values={{ time: timestamp ? timestamp : 'Unknown' }}
-                      />
-                    </p>
-                    <EuiButton type="primary" size="s" onClick={onClickHandler}>
-                      <FormattedMessage id="shardMap.buttonText" defaultMessage="Get data" />
-                    </EuiButton>
-                  </EuiText>
-                </EuiPageContentBody>
-              </EuiPageContent>
-            </EuiPageBody>
-          </EuiPage>
-        </>
-      </I18nProvider>
+      <ApolloProvider client={client}>
+        <I18nProvider>
+          <>
+            <navigation.ui.TopNavMenu appName={PLUGIN_ID} showSearchBar={false} />
+            <EuiPage>
+              <EuiPageBody>
+                <EuiPageContent>
+                  <EuiPageContentBody>
+                    <Table />
+                  </EuiPageContentBody>
+                </EuiPageContent>
+              </EuiPageBody>
+            </EuiPage>
+          </>
+        </I18nProvider>
+      </ApolloProvider>
     </Router>
   );
 };
